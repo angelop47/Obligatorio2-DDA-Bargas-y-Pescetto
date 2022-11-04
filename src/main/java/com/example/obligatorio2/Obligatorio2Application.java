ae29312df7e3a061dedd86b1b89f71314f9af184
angelop47 <pescettoangelo@gmail.com>
2022-11-04 20:36:13 -0300

    first commit

---
+++ src/main/java/com/example/obligatorio2/Obligatorio2Application.java
@@ -0,0 +1,13 @@
+package com.example.obligatorio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Obligatorio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Obligatorio2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/obligatorio2/Obligatorio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obligatorio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Obligatorio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
